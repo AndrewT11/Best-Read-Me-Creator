740eff7f70094529c90939977a9e9d7d4665f54c
Andrew Tran <tranandrew@gmail.com>
2021-10-08 18:36:27 -0500

    Added questions, created fs.writefile code

--- Develop/index.js
+++ Develop/index.js
@@ -4,9 +4,41 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [
+const questions = 
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    },
+    {
+        type: 'input',
+        name: 'location',
+        message: 'Where are you located?'
+    },
+    {
+        type: 'input',
+        name: 'hobby',
+        message: 'What is your favorite hobby?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please input your Github Username'
+    },
+    {
+        type: 'input',
+        name: 'linkedin',
+        message: 'Please input your LinkedIn Username'
+    },
+    
+])
+.then((answers) => {
+    const readMePageContent = generateReadMe(answers);
 
-];
+        fs.writeFile('readMe.md', readMePageContent, (err) => err ? console.log(err) : console.log('Successfully created index.html!')
+        )
+})
 
 // TODO: Create a function to write README file
 function writeToFile(readMe.md, data) {
@@ -15,7 +47,7 @@ function writeToFile(readMe.md, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    
+
 }
 
 // Function call to initialize app
