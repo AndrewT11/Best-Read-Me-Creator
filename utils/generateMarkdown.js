ac06469b3d664c6541a8c83cb7c48c2f6a635c11
Andrew Tran <tranandrew@gmail.com>
2021-10-08 21:23:32 -0500

    4 license badge links for use later

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function renderLicenseBadge(license) {
+//   [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+//   [![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+//   [![License: APACHE 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+//   [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -50,7 +56,7 @@ function generateMarkdown(data) {
   Contact me by the following:
 
   GitHub Link: [${data.github}](http://www.github.com/${data.github})
-  
+
   LinkedIn: [${data.linkedin}](http://www.linkedin.com/${data.linkedin})
 `;
 }
